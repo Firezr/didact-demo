0ec8e259c823d5c4309a59d0842c3917771e767c
zengrui <zengr666@163.com>
2022-07-16 22:28:37 +0800

    feat: add function comps and hooks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import Didact from "./didact";
 
 /** @jsx Didact.createElement */
-const container = document.getElementById("root");
-
-const updateValue = (e) => {
-  rerender(e.target.value);
-};
-
-const rerender = (value) => {
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
+function Counter() {
+  const [state, setState] = Didact.useState(1);
+  return (
+    <h1 onClick={() => setState(c => c + 1)} style="user-select: none">
+      Count: {state}
+    </h1>
   );
-  Didact.render(element, container);
-};
+}
+const element = <Counter />;
+const container = document.getElementById("root");
+Didact.render(element, container);
 
-rerender("World");
